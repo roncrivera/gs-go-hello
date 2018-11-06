f1afc588eb238b1abf013e3d11b65c061e01e2b7
Ron Rivera <roncrivera@gmail.com>
2018-11-06 22:53:32 +1100

    Import stringutil.

--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/roncrivera/stringutil"
+)
 
 func main() {
     fmt.Println("Hello, world.")
+    fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
